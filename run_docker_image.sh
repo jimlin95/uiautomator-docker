523943a904d3a371232c941e80e7b05ec883b9fa
Jim Lin <jim_lin@quantatw.com>
2015-05-05 10:49:31 +0800

    change to daemon mode, change the name to uiautomator
    Signed-off-by: Jim Lin <jim_lin@quantatw.com>

--- run_docker_image.sh
+++ run_docker_image.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #run docker
-docker run --name uiautomator-sdk --privileged -v /dev/bus/usb:/dev/bus/usb -v /tmp/.X11-unix:/tmp/.X11-unix -v /home/jim/work/uiautomator:/src -ti -p 2222:22 jimlin95/uiautomator 
+docker run --name uiautomator --privileged -v /dev/bus/usb:/dev/bus/usb -v /tmp/.X11-unix:/tmp/.X11-unix -v /home/jim/work/uiautomator:/src -d -p 2222:22 jimlin95/uiautomator 
